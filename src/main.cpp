1d5b54347fb7fe0cc20ba248c59228f90f2b0730
atulbhartiproff <atulbhartiproff@gmail.com>
2025-05-22 23:36:38 +0530

    Reading CSV and checking if it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Trading Platform - Does it work??????" <<endl;
+    cout<<"Well Market Data hmmmm..."<<endl;
+    ifstream file("../src/market_data/market_data.csv");
+    string line;
+
+    if(file.is_open()){
+        while(getline(file,line)){
+            cout<<line<<endl;
+        }
+        file.close();
+    }
+    else
+    {
+        cout<<"That is weird, Where da file it?";
+    }
     return 0;
 }
